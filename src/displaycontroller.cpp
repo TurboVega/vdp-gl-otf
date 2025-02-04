61853d2688bd47f18fd0ec4913885a7151a74b53
Curtis Whitley <restday7@gmail.com>
2025-02-04 14:40:29 -0500

    remove some debug code

--- src/displaycontroller.cpp
+++ src/displaycontroller.cpp
@@ -693,10 +693,8 @@ void IRAM_ATTR BitmappedDisplayController::showSprites(Rect & updateRect)
 {
   //m_spritesHidden = false;
 }
-extern "C" { uint32_t pix[8]; bool isset[8]; }
-void BitmappedDisplayController::drawSpriteScanLine(uint8_t * pixelData, int scanRow, int scanWidth, int viewportHeight) {
-    //if (m_spritesHidden) return;
 
+void BitmappedDisplayController::drawSpriteScanLine(uint8_t * pixelData, int scanRow, int scanWidth, int viewportHeight) {
     // normal sprites
     pixelData[13]=((0x30) & 0x3F) | m_HVSync;
     int spritesCnt = spritesCount();
@@ -731,19 +729,11 @@ void BitmappedDisplayController::drawSpriteScanLine(uint8_t * pixelData, int sca
         auto pos = spriteX + offsetX;
 
         while (drawWidth--) {
-          auto word = (*((uint32_t*)src))^0xFFFFFFFF;
-          int k;
-          for (k=0;k<8;k++) {
-            if (!isset[k]) {pix[k]=word;isset[k]=true;break;}
-            if (pix[k]==word) break;
-          }
-
           if (src[3]) {
             auto r = src[0] >> 6;
             auto g = (src[1] >> 6) << 2;
             auto b = (src[2] >> 6) << 4;
             pixelData[pos ^ 2] = r | g | b | m_HVSync;
-
           }
           src += 4;
           pos++;
